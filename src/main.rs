9410ad290100a7d8d05e3d074fb91273e37bf8a0
Huzaifa Hatim <huzaifahatim3402@gmail.com>
2021-10-01 16:39:40 +0500

    Update Version

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,71 @@ fn main() {
 
     } 
 
-println!("NAME: {} age: {} rollno: {}",name , rollno, age);
+
+
+
+
+    /*if args[1].as_str() == "--name" {
+        let name = &args[2];
+    }
+    else if args[3].as_str() ==  "--age" {
+        let age = &args[4];
+        
+    }
+    else if args[5].as_str() == "--rollno" {
+        let rollno = &args[6];
+
+    }*/
+    
+     
+
+
+
+
+
+       
+
+    
+
+    /*let name = &args[2];
+    let age = &args[4];
+    let rollno = &args[6];*/
+
+    println!("NAME: {} age: {} rollno: {}",name , rollno, age);
+
+
+
+
+
+
+    
+
+
+/*    println!("ENTER YOUR NAME :");
+    let mut name = String::new();
+    name.clear();
+    io::stdin().read_line(&mut name).unwrap();
+    
+
+
+    println!("ENTER YOUR ROLL NO :");
+    let mut rollno = String::new();
+    rollno.clear();
+    io::stdin().read_line(&mut rollno).unwrap();
    
+
+    println!("ENTER YOUR AGE :");
+    let mut age = String::new();
+    io::stdin().read_line(&mut age).unwrap();
+    
+
+    println!("Your Name is {}", name);
+    println!("Your Rollno is {}", rollno);
+    println!("Your Age is {}", age);*/
+
+
+
+
+    
     
 }
